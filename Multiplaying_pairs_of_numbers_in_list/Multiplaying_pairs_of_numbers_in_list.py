5d3180ec7700581051517e1150ee59939f38ee8f
Dmitry Tikhonov <tdmv1209@gmail.com>
2022-06-01 21:40:17 +0300

    Created a program that multiplays pairs of number in list

---
+++ Multiplaying_pairs_of_numbers_in_list/Multiplaying_pairs_of_numbers_in_list.py
@@ -0,0 +1,16 @@
+#from ast import Mult
+import Create_list_random_int_from_0_to_9 as create
+from math import ceil
+
+
+def Multiplaying_pairs_of_numbers_in_list (any_list):
+    new_list = []
+    for item in range (ceil(len(any_list) / 2)):
+        new_list.append(any_list[item] * any_list[len(any_list) - 1 - item])
+    return new_list
+
+length = int(input('Введите количество элементов в списке: '))
+my_list = create.Create_list_random_int_from_0_to_9(length)
+print(my_list)
+new_list = Multiplaying_pairs_of_numbers_in_list(my_list)
+print(new_list)
