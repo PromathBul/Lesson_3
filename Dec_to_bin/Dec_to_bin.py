4f8cdf73e3ec8d9d952e54eadb7a23b260653474
Dmitry Tikhonov <tdmv1209@gmail.com>
2022-06-02 21:31:33 +0300

    Created a program that converts decimal to binary

---
+++ Dec_to_bin/Dec_to_bin.py
@@ -0,0 +1,12 @@
+from math import log2
+
+def Dec_to_bin (number):
+    txt_bin = ''
+    while number > 0:
+        txt_bin = str(number % 2) + txt_bin
+        number //= 2
+    return txt_bin
+
+number = int(input('Введите число: '))
+bin_number = Dec_to_bin(number)
+print(bin_number)
